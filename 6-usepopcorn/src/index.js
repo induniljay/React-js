29539f9252fd6e71fc780ee2ac28c7884d9cf368
induniljay <induniljay99@gmail.com>
2024-01-31 00:55:01 +0530

    7:how react works part completed

---
+++ 6-usepopcorn/src/index.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App.js";
+// import "./index.css";
+import StarRating from "./StarRating.js";
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating onSetRating={setMovieRating} />
+//       {movieRating > 0 && <p>This movie is rated {movieRating}</p>}
+//     </div>
+//   );
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <StarRating messages={["Terrible", "bad", "Okay", "Good", "Amazing"]} />
+    <StarRating maxRating={10} color="green" />
+    <StarRating defaultRating={4} />
+    {/* <Test /> */}
+  </React.StrictMode>
+);
